b63fc57fab94482d0131e4d5cf4ec02f298d8b9f
Jason <jbockover7@gmail.com>
2024-07-30 14:11:44 -0700

    Updated game.py

--- game.py
+++ game.py
@@ -1,28 +1,83 @@
 # Simple pygame program 
 
-# Import and initialize the pygame
+# Import the pygame module
 import pygame
+
+# Import pygame.locals for easier access to key locals
+from pygame.locals import (
+        K_UP,
+        K_DOWN,
+        K_LEFT,
+        K_RIGHT,
+        K_ESCAPE,
+        KEYDOWN,
+        QUIT,
+)
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Define a Player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+# Initialize pygame 
 pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
-# Run until the user asks to quit
+# Instantiate player. Right now, this is just a rectangle.
+player = Player()
+
+# Variable to keep the main loop running 
 running = True
-while running:
 
-    # Did the user click the window close button?
+# Main loop
+while running:
+    # Loop at every event in the queue
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+        
+        #Did the user click the window close button?
+        elif event.type == QUIT:
             running = False
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+    # Fill the screen with black
+    screen.fill((0, 0, 0))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # Draw the player on the screen
+    screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    
+    # Update the display
+    pygame.display.flip()
 
-    # Flip the display
+    # Create a surface and pass in a tuple containing its length and width
+    surf = pygame.Surface((50, 50))
+
+    # Give the surface a color to separate it from the background
+    surf.fill((0, 0, 0))
+    rect = surf.get_rect()
+
+    #Put the center of surf at the center of the display
+    surf_center = (
+            (SCREEN_WIDTH-surf.get_width())/2,
+            (SCREEN_HEIGHT-surf.get_height())/2
+    )
+
+    # Draw surf at the new coordinates
+    screen.blit(surf, surf_center)
     pygame.display.flip()
 
 # Done! Time to quit.
